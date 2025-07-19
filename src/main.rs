d190aaa660e66f065051db404edd2c0b7422db9d
Atom <tbjong2000@hotmail.com>
2025-07-19 20:52:03 +0800

    update variable names to make more sense

--- src/main.rs
+++ src/main.rs
@@ -11,32 +11,32 @@ fn add_arrays<F: Float>(lhs: &Array<F>, rhs: &Array<F>, out: &mut Array<F>) {
 
 fn launch<R: Runtime, F: Float + CubeElement>(device: &R::Device) {
     let client = R::client(device);
-    let lhs = &[-1., 10., 1., 5.];
-    let rhs = &[-1., 10., 1., 5.];
-    let len = lhs.len();
+    let lhs_cpu = &[-1., 10., 1., 5.];
+    let rhs_cpu = &[-1., 10., 1., 5.];
+    let len = lhs_cpu.len();
 
     // Create handles to data on the accelerator device
-    let output = client.empty(len * core::mem::size_of::<f32>());
-    let lhs = client.create(f32::as_bytes(lhs));
-    let rhs = client.create(f32::as_bytes(rhs));
+    let output_gpu = client.empty(len * core::mem::size_of::<f32>());
+    let lhs_gpu = client.create(f32::as_bytes(lhs_cpu));
+    let rhs_gpu = client.create(f32::as_bytes(rhs_cpu));
 
     unsafe {
-        let array_lhs = ArrayArg::from_raw_parts::<f32>(&lhs, len, 1);
-        let array_rhs = ArrayArg::from_raw_parts::<f32>(&rhs, len, 1);
-        let array_output = ArrayArg::from_raw_parts::<f32>(&output, len, 1);
+        let array_arg_lhs = ArrayArg::from_raw_parts::<f32>(&lhs_gpu, len, 1);
+        let array_arg_rhs = ArrayArg::from_raw_parts::<f32>(&rhs_gpu, len, 1);
+        let array_arg_output = ArrayArg::from_raw_parts::<f32>(&output_gpu, len, 1);
         add_arrays::launch_unchecked::<F, R>(
             &client,
             CubeCount::Static(1, 1, 1),     // Create 1 cuboid
             CubeDim::new(len as u32, 1, 1), // Make cuboid one long row of threads
-            array_lhs,
-            array_rhs,
-            array_output,
+            array_arg_lhs,
+            array_arg_rhs,
+            array_arg_output,
         );
     }
 
-    let bytes = client.read_one(output.clone().binding());
-    let output = f32::from_bytes(&bytes);
-    println!("{output:?}");
+    let bytes = client.read_one(output_gpu.clone().binding());
+    let output_cpu = f32::from_bytes(&bytes);
+    println!("{output_cpu:?}");
 }
 
 fn main() {
