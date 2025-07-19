84b1b03ec0b93b1ec68ed57552b3741844496e20
Atom <tbjong2000@hotmail.com>
2025-07-19 20:54:02 +0800

    Update with comments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use cubecl::prelude::*;
 
+/// Kernel for adding two 1D arrays, and placing result into output array.
 #[cube(launch_unchecked)]
 fn add_arrays<F: Float>(lhs: &Array<F>, rhs: &Array<F>, out: &mut Array<F>) {
     if UNIT_POS_X < lhs.len() {
@@ -9,6 +10,7 @@ fn add_arrays<F: Float>(lhs: &Array<F>, rhs: &Array<F>, out: &mut Array<F>) {
     }
 }
 
+/// Set up client/context. Create data on cpu -> copy to gpu then run kernel
 fn launch<R: Runtime, F: Float + CubeElement>(device: &R::Device) {
     let client = R::client(device);
     let lhs_cpu = &[-1., 10., 1., 5.];
@@ -27,13 +29,15 @@ fn launch<R: Runtime, F: Float + CubeElement>(device: &R::Device) {
         add_arrays::launch_unchecked::<F, R>(
             &client,
             CubeCount::Static(1, 1, 1),     // Create 1 cuboid
-            CubeDim::new(len as u32, 1, 1), // Make cuboid one long row of threads
+            CubeDim::new(len as u32, 1, 1), // Make cuboid one long row of, then we can run kernel
+            // on each unit
             array_arg_lhs,
             array_arg_rhs,
             array_arg_output,
         );
     }
 
+    // Read back data from gpu to cpu
     let bytes = client.read_one(output_gpu.clone().binding());
     let output_cpu = f32::from_bytes(&bytes);
     println!("{output_cpu:?}");
